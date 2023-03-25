86e13789ee0e87a5ae1c40534e661f8d6968d91b
yjh1008 <exesea12@gmail.com>
2023-03-25 15:45:37 +0900

    B6588

--- Beakjoon/src/DAY47/에라토스테네스의채/Main.java
+++ Beakjoon/src/DAY47/에라토스테네스의채/Main.java
@@ -1,2 +1,32 @@
-package DAY47.에라토스테네스의채;public class Main {
+package DAY47.에라토스테네스의채;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        boolean[]arr = new boolean[n+1];
+        int count = 0;
+        for(int i=2;i<=n;i++) {
+                for(int j=i;j<=n;j+=i) {
+                    if(!arr[j]) {
+                        arr[j] = true;
+                        count += 1;
+                    }
+                    if(count == k){
+                        System.out.println(j);
+                        break;
+                    }
+            }
+        }
+
+
+    }
 }
---
+++ Beakjoon/src/DAY6588/골드바흐의추측/Main.java
@@ -0,0 +1,42 @@
+package DAY6588.골드바흐의추측;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static String wrongAnswer = "Goldbach's conjecture is wrong";
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n;
+        boolean arr[] = new boolean[10000001];
+        arr[0] = true;
+        arr[1] = true;
+
+        for(int i=2;i<=1000000;i++) {
+            if(!arr[i]) {
+                for(int j=i+i;j<=1000000;j+=i) {
+                    arr[j] = true;
+                }
+            }
+        }
+        boolean chk;
+        //System.out.println(arr[4]);
+        while(true) {
+            n = Integer.parseInt(br.readLine());
+            if(n == 0) break;
+            chk = false;
+            for(int i=2;i<=n/2;i++) {
+
+                if(!arr[i] && !arr[n-i]) {
+                    sb.append(n).append(" = ").append(i).append(" + ").append(n-i).append("\n");
+                    chk = true;
+                    break;
+                }
+            }
+            if(!chk) sb.append(wrongAnswer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
