a71b076d97f48b041e7280c7f617475b2f53a0d6
yjh1008 <exesea12@gmail.com>
2023-04-10 22:42:09 +0900

    연속된 부분수열의 합 수정

--- Programmers/src/DAY36/연속된부분수열의합/Solution.java
+++ Programmers/src/DAY36/연속된부분수열의합/Solution.java
@@ -1,6 +1,4 @@
-package DAY36.연속된부분수열의합;
-
-public class Solution {
+class Solution {
     public static int[] solution(int[] sequence, int k) {
         int[] answer = new int[2];
         int len = Integer.MAX_VALUE;
@@ -9,37 +7,24 @@ public class Solution {
         if(sum == k) {
             return new int[]{lt,rt};
         }
-        while(rt<sequence.length-1 && lt<sequence.length-1) {
-           while(rt<=sequence.length-1 && sum < k) {
-               sum+=sequence[++rt];
-           }
-
-           while(lt<=sequence.length-1 && sum > k) {
-               sum -= sequence[lt++];
-           }
-
-           while(sum == k) {
-               if(answer[1] == 0) {
-                   answer = new int[]{lt, rt};
-               }else {
-                   if(answer[1] - answer[0] > rt-lt){
-                       answer = new int[]{lt, rt};
-                   }
-               }
-               sum -= sequence[lt++];
-           }
-            if(answer[1] != 0 && answer[1] - answer[0] == 0) return answer;
+        while(rt<sequence.length) {
+            if(sum < k) {
+                if(++rt < sequence.length) {
+                    sum += sequence[rt];
+                }
+            }else if(sum > k) {
+                sum -= sequence[lt++];
+            }else {
+                if(rt - lt < len){
+                    len = rt - lt;
+                    answer = new int[]{lt, rt};
+                }
+                sum -= sequence[lt++];
+            }
         }
 
+
+        //System.out.print(answer[0]+" "+answer[1]);
         return answer;
-    }
-    public static void main(String[] args) {
-        int[] result = solution(
-                new int[]{7,5,5,1,1,50,50},
-                100
-        );
-        System.out.print(
-                result[0]+" "+result[1]
-        );
     }
 }
