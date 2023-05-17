aaa2651a68e06c2b75de0e22f318e45a67380e73
yjh1008 <exesea12@gmail.com>
2023-05-17 20:41:28 +0900

    B10999

---
+++ Beakjoon/src/DAY61/B10988/Solution.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('./index.txt').toString(); 
+const len = input.length/2;
+let left = input.slice(0,parseInt(len));
+let right = input.slice( Number.isInteger(len) ? len: parseInt(len)+1).split('').reverse().join('');
+
+if(left === right) console.log(1);
+else console.log(0);
