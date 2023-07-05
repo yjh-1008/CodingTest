723369be9dbde7ed343c1063b2d5434bf74c6001
yjh1008 <exesea12@gmail.com>
2023-07-06 00:44:11 +0900

    Bitmasking 예제

--- Beakjoon/src/DAY92/BitMasking/example.js
+++ Beakjoon/src/DAY92/BitMasking/example.js
@@ -1,12 +1,26 @@
 
 
-function main() {
-  let bit = 18;
-  //비트 끄기
- // S& = ~(S<<idx)
-  let idx = 1;
-  bit &= ~(1<<idx);
-  console.log(bit)
-}
+// function main() {
+//   let bit = 18;
+//   //비트 끄기
+//  // S& = ~(S<<idx)
+//   let idx = 1;
+//   bit &= ~(1<<idx);
+//   console.log(bit)
+// }
 
+
+function main() {
+  let arr = ["사과","딸기","포도","배"];
+  let n  =arr.length;
+  for(let i=0; i< (1<<n); i++) {
+    let ret = "";
+    for(let j=0;j<n;j++) {
+      if(i & (1<<j)) {
+          ret += arr[j]+" ";
+      }
+    }
+    console.log(ret)
+  }
+}
 main()
