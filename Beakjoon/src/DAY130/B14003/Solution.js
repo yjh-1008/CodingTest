07185c9298d385135f2108a446ed763174114159
yjh1008 <exesea12@gmail.com>
2023-08-27 15:27:30 +0900

    B14003-2

--- Beakjoon/src/DAY130/B14003/Solution.js
+++ Beakjoon/src/DAY130/B14003/Solution.js
@@ -1,55 +1,108 @@
 const fs = require('fs');
-const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n").map(v => v.split(' ').map(Number))
+const input = fs.readFileSync("./index.txt").toString().trim().split("\n").map(v => v.split(' ').map(Number))
 const [N] = input[0];
 const arr = input[1];
 
+function Solution() {
 
-function binarySearch(left, right, target) {
-  let mid;
-  while (left < right) {
-    mid = Math.floor((left + right) / 2);
+  const binarySearch = (lis, s, e, n) => {
+    let mid;
+    while(s<=e) {
+      console.log("here")
+      mid = Math.floor((s+e)/2);
+      if(lis[mid] < n) {
+        s = mid+1;
+      }else {
+        e = mid-1;
+      }
+    }
 
-    if (lis[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid
+    return e;
+  }
+
+  let lis=[];
+  let idxs=[];
+  lis[0] = arr[0];
+  idxs[0] = 0;
+  let j = 0;
+  for(let i=1; i<N; i++) {
+    if(lis[j] < arr[i]) {
+      lis[++j] = arr[i];
+      idxs[i] = j;
+    }else {
+      let idx = binarySearch(lis, 0, j, arr[i]);
+      
+      lis[idx] = arr[i];
+      idxs[i] = idx;
     }
   }
-  return right;
-}
-
-let lis = [];
-let records = [];
-records[0] = 0;
-let j = 0;
-lis[0] = arr[0];
-let i = 1;
-
-while (i < N) {
-
-  if (lis[j] < arr[i]) {
-    lis[++j] = arr[i];
-    records[i] = j;
-  } else {
-    let idx = binarySearch(0, j, arr[i]);
-    lis[idx] = arr[i]
-    records[i] = idx;
+  
+  let ret = [];
+  let max = Math.max(...idxs), maxIdx = idxs.indexOf(max);
+  for(let i=maxIdx;i>=0;i--) {
+    if(max === idxs[i]) {
+      ret.push(arr[i]);
+      max--;
+    }
+    if(max < 0) break;
   }
-  i++;
+  console.log(ret.length)
+  console.log(ret.reverse().join(' '))
 }
-let answer = [];
 
-let max = Math.max(...records)
-const maxIdx = records.indexOf(max)
 
-for (let i = maxIdx; i >= 0; i--) {
-  if (records[i] == max) {
-    answer.push(arr[i]);
-    max--;
+Solution();
+const fs = require('fs');
+const input = fs.readFileSync("./index.txt").toString().trim().split("\n").map(v => v.split(' ').map(Number))
+const [N] = input[0];
+const arr = input[1];
+
+function Solution() {
+
+  const binarySearch = (lis, s, e, n) => {
+    let mid;
+    while(s<=e) {
+      console.log("here")
+      mid = Math.floor((s+e)/2);
+      if(lis[mid] < n) {
+        s = mid+1;
+      }else {
+        e = mid-1;
+      }
+    }
+
+    return e;
   }
-  if (max < 0) break;
+
+  let lis=[];
+  let idxs=[];
+  lis[0] = arr[0];
+  idxs[0] = 0;
+  let j = 0;
+  for(let i=1; i<N; i++) {
+    if(lis[j] < arr[i]) {
+      lis[++j] = arr[i];
+      idxs[i] = j;
+    }else {
+      let idx = binarySearch(lis, 0, j, arr[i]);
+      
+      lis[idx] = arr[i];
+      idxs[i] = idx;
+    }
+  }
+  
+  let ret = [];
+  let max = Math.max(...idxs), maxIdx = idxs.indexOf(max);
+  for(let i=maxIdx;i>=0;i--) {
+    if(max === idxs[i]) {
+      ret.push(arr[i]);
+      max--;
+    }
+    if(max < 0) break;
+  }
+  console.log(ret.length)
+  console.log(ret.reverse().join(' '))
 }
 
 
-console.log(answer.length)
-console.log(answer.reverse().join(' '))
+Solution();
