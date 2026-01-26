2b79aa245313d336cf92529e573ed7dbd8e5cacb
yjh1008 <exesea12@gmail.com>
2026-01-26 21:53:27 +0900

    LeetCOde: -11(내가 해결)

--- LeetCode/11.js
+++ LeetCode/11.js
@@ -3,13 +3,19 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left = 0, right = height.length-1;
-    let answer = -Infinity
-    while(left <right) {
-        const area = Math.min(height[left], height[right]) * (right-left);
+    let left = 0;right = height.length-1;
+
+    let answer = -Infinity;
+
+    //너비 = Math.abs(right - left) * Math.min(height[left], height[right]);
+
+    while(left<right) {
+        const area = Math.abs(right - left) * Math.min(height[left], height[right]);
+        // console.log( Math.abs(right - left), Math.min(height[left], height[right]))
         answer = Math.max(answer, area)
-       if(height[left] > height[right]) right--;
-        else left++;
+        if(height[left] < height[right]) left++;
+        else right--;
     }
+
     return answer;
 };
